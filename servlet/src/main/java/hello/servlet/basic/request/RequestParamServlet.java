7198c87f1b80fedfb97585237d5c910f39da440d
s-yeong <syxxng25@gmail.com>
2022-12-31 02:58:54 +0900

    서블릿: HTTP 요청 데이터 - POST HTML Form

--- servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -11,7 +11,11 @@ import java.util.Enumeration;
 
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
-
+    /**
+     * GET - 쿼리 파라미터,   POST - HTML Form
+     * 1. 파라미터 전송 기능 => http://localhost:8080/request-param?username=hello&age=20
+     * 2. 동일한 파라미터 전송 기능 => http://localhost:8080/request-param?username=hello&age=20&username=kim
+    **/
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
