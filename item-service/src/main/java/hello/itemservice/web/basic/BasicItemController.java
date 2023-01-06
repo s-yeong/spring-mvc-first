16772c68fc6d19990d8d16e7e61946afefc59d88
s-yeong <syxxng25@gmail.com>
2023-01-06 15:16:44 +0900

    스프링 MVC - 웹 페이지 만들기: 상품 목록 - 타임리프

---
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,41 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        // 모든 상품 조회
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+
+        // 뷰 템플릿 호출
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터
+     */
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
